4955e68c97f807484f2944c7aa7e43e7cc4dfc30
Brawler <52391950+BrawlerAce@users.noreply.github.com>
2025-02-01 23:18:20 -0600

    Add config options (but for real this time)

--- p3ppc.qolcollection/Mod.cs
+++ p3ppc.qolcollection/Mod.cs
@@ -2,6 +2,11 @@
 using p3ppc.qolcollection.Template;
 using Reloaded.Hooks.ReloadedII.Interfaces;
 using Reloaded.Mod.Interfaces;
+using CriFs.V2.Hook;
+using CriFs.V2.Hook.Interfaces;
+using PAK.Stream.Emulator;
+using PAK.Stream.Emulator.Interfaces;
+using System.Diagnostics;
 
 namespace p3ppc.qolcollection
 {
@@ -50,6 +55,9 @@ namespace p3ppc.qolcollection
             _configuration = context.Configuration;
             _modConfig = context.ModConfig;
 
+#if DEBUG
+            Debugger.Launch();
+#endif
 
             // For more information about this template, please see
             // https://reloaded-project.github.io/Reloaded-II/ModTemplate/
@@ -58,6 +66,38 @@ namespace p3ppc.qolcollection
             // and some other neat features, override the methods in ModBase.
 
             // TODO: Implement some mod logic
+
+            var criFsController = _modLoader.GetController<ICriFsRedirectorApi>();
+            if (criFsController == null || !criFsController.TryGetTarget(out var criFsApi))
+            {
+                _logger.WriteLine($"criFSController returned as null! p3ppc.qolcollection won't work properly :(", System.Drawing.Color.Red);
+                return;
+            }
+
+            var PakEmulatorController = _modLoader.GetController<IPakEmulator>();
+            if (PakEmulatorController == null || !PakEmulatorController.TryGetTarget(out var _PakEmulator))
+            {
+                _logger.WriteLine($"PakEmulatorController returned as null! p3ppc.qolcollection won't work properly :(", System.Drawing.Color.Red);
+                return;
+            }
+
+            var modDir = _modLoader.GetDirectoryForModId(_modConfig.ModId);
+
+            this._modLoader.OnModLoaderInitialized += () =>
+            {
+            };
+
+            if (_configuration.UsefulDesc == true)
+            {
+                //criFsApi.AddProbingPath("UD1");
+                _PakEmulator.AddDirectory(Path.Combine(modDir, "UD1", "PAK"));
+            }
+
+            if (_configuration.UsefulDesc == false)
+            {
+                //criFsApi.AddProbingPath("UD0");
+                _PakEmulator.AddDirectory(Path.Combine(modDir, "UD0", "PAK"));
+            }
         }
 
         #region Standard Overrides
